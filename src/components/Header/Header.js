0982f46c97c53571f01b6e6b23612dbcf71a3d01
Roger <rdpender3@gmail.com>
2018-10-19 20:36:23 -0700

    fixing some styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./Header.css";
 
-const Header = props => <div>
+const Header = props => <div className="header">
 
   <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <p className="navbar-brand">Clicky Game</p>
-    <h3>{props.message}</h3>
-    <p className="nav-item nav-link">Score: {props.userScore || 0} | High Score: {props.highScore}</p>
+   <p className="navbar-brand">Clicky Game</p>
+   <h3>{props.message}</h3>
+   <p className="nav-item nav-link float-right">Score: {props.userScore || 0} | High Score: {props.highScore}</p>
   </nav>
 </div>
 
--- src/components/ImageCard/ImageCard.js
+++ src/components/ImageCard/ImageCard.js
@@ -3,11 +3,13 @@ import "./ImageCard.css";
 
 const ImageCard = props => (
 <div className="card-container">
+<span onClick={() => props.selectImage(props.id)}>
   <div className="card float-left">
-  <span onClick={() => props.selectImage(props.id)}>
+
   <img className="card-img-top content img-container" src={props.image} alt={props.name} />
-  </span>
+
   </div>
+  </span>
   </div>
 );
 
