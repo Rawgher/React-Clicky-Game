173382cc17f63d1188ff1df677ba89efeca91905
Roger <rdpender3@gmail.com>
2018-10-20 14:28:06 -0700

    Updates

---
+++ precache-manifest.459b9ba39479ada11739dfde6d2e4936.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "de0f377bd9bffb58890788aaea1e1da5",
+    "url": "/react-clicky-game/static/media/spiderman-logo.de0f377b.jpg"
+  },
+  {
+    "revision": "65f2325f83870e01a84fd9b55a907030",
+    "url": "/react-clicky-game/static/media/modalbackground.65f2325f.jpg"
+  },
+  {
+    "revision": "55abe30a270b3be75741",
+    "url": "/react-clicky-game/static/js/runtime~main.55abe30a.js"
+  },
+  {
+    "revision": "9bdaa19ec5bf258c818a",
+    "url": "/react-clicky-game/static/js/main.9bdaa19e.chunk.js"
+  },
+  {
+    "revision": "cea273dba9a1ea5b36e1",
+    "url": "/react-clicky-game/static/js/1.cea273db.chunk.js"
+  },
+  {
+    "revision": "9bdaa19ec5bf258c818a",
+    "url": "/react-clicky-game/static/css/main.9892c3e7.chunk.css"
+  },
+  {
+    "revision": "a21df7439cfd2a27c8b3090b6b96d80a",
+    "url": "/react-clicky-game/index.html"
+  }
+];
